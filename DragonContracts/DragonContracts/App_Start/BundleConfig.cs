808b810720d476beb8f12e6127bf181a3696b67b
Petr Vavro <BlackSun.Petr@gmail.com>
2013-10-16 01:31:22 +0200

    Localized calendar for SignedOn field

--- DragonContracts/DragonContracts/App_Start/BundleConfig.cs
+++ DragonContracts/DragonContracts/App_Start/BundleConfig.cs
@@ -12,7 +12,8 @@ namespace DragonContracts
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+                        "~/Scripts/jquery-ui-{version}.js",
+                        "~/Scripts/jquery-ui-i18n.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
